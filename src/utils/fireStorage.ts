23ef8039d27e3d71316cd96c40f6a5525e77ad6a
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-04-06 19:40:44 -0300

    Pontos que preciso ver com o jeanzito

--- src/utils/fireStorage.ts
+++ src/utils/fireStorage.ts
@@ -1,4 +1,4 @@
-import { CloudImagesArrayProps, ImageFile } from '@/types/VehiclesTypes';
+import { CloudImagesArrayProps, ImageFile } from "@/types/VehiclesTypes";
 import {
   deleteObject,
   getDownloadURL,
@@ -7,10 +7,10 @@ import {
   StorageReference,
   uploadBytes,
   uploadBytesResumable,
-} from 'firebase/storage';
-import { storage } from 'firebaseEnv';
-import { Dispatch, SetStateAction } from 'react';
-import { toast } from 'react-toastify';
+} from "firebase/storage";
+import { storage } from "./firebaseEnv";
+import { Dispatch, SetStateAction } from "react";
+import { toast } from "react-toastify";
 
 export const fetchMainImageUrl = async (
   vehicleId: string,
@@ -19,15 +19,17 @@ export const fetchMainImageUrl = async (
 ) => {
   try {
     setLoadingState(true);
-    const response = await getDownloadURL(ref(storage, `${vehicleId}/mainImage`));
+    const response = await getDownloadURL(
+      ref(storage, `${vehicleId}/mainImage`)
+    );
     setLoadingState(false);
     setState(response);
   } catch ({ message, name }) {
     setLoadingState(false);
     toast(
-      'Houve um erro ao carregar a(s) imagem(s) principal(ais) do(s) veiculo(s):\n' +
+      "Houve um erro ao carregar a(s) imagem(s) principal(ais) do(s) veiculo(s):\n" +
         `${message}:${name}`,
-      { className: 'error' }
+      { className: "error" }
     );
   }
 };
@@ -38,12 +40,18 @@ export const fetchImageUrl = async (
   setState: Dispatch<SetStateAction<string>>
 ) => {
   try {
-    const response = await getDownloadURL(ref(storage, `${vehicleId}/${cloudImage.name}`));
+    const response = await getDownloadURL(
+      ref(storage, `${vehicleId}/${cloudImage.name}`)
+    );
     setState(response);
   } catch ({ name, message }) {
-    toast('Houve um erro ao carregar a(s) imagem(s) do(s) veiculo(s):\n' + `${message}:${name}`, {
-      className: 'error',
-    });
+    toast(
+      "Houve um erro ao carregar a(s) imagem(s) do(s) veiculo(s):\n" +
+        `${message}:${name}`,
+      {
+        className: "error",
+      }
+    );
   }
 };
 
@@ -56,16 +64,18 @@ export const setManyImagesUrls = (
   try {
     setLoadingState(true);
     cloudImageArray.forEach(async (image) => {
-      await getDownloadURL(ref(storage, `${vehicleId}/${image.name}`)).then((url: string) => {
-        setState((state: string[]) => [...state, `${url}||${image.name}`]);
-      });
+      await getDownloadURL(ref(storage, `${vehicleId}/${image.name}`)).then(
+        (url: string) => {
+          setState((state: string[]) => [...state, `${url}||${image.name}`]);
+        }
+      );
     });
     setLoadingState(false);
   } catch ({ message, name }) {
     toast(
-      'Houve um erro ao carregar a lista de URL das imagens de todos os veiculos:\n' +
+      "Houve um erro ao carregar a lista de URL das imagens de todos os veiculos:\n" +
         `${message}:${name}`,
-      { className: 'error' }
+      { className: "error" }
     );
   }
 };
@@ -82,9 +92,9 @@ export const fetchImagesReferenceList = async (
     setState((state) => response.items.reverse());
   } catch ({ message, name }) {
     toast(
-      'Houve um erro ao carregar a lista de referencia das imagens de todos os veiculos:\n' +
+      "Houve um erro ao carregar a lista de referencia das imagens de todos os veiculos:\n" +
         `${message}:${name}`,
-      { className: 'error' }
+      { className: "error" }
     );
   }
 };
@@ -96,13 +106,17 @@ export const deleteImageByStorageReference = async (
   setState: Dispatch<SetStateAction<StorageReference[]>>
 ) => {
   try {
-    const deleteFile = deleteObject(ref(storage, `${vehicleId}/${cloudImageName}`));
-    const remainingImages = cloudImages.filter((image) => image.name !== cloudImageName);
+    const deleteFile = deleteObject(
+      ref(storage, `${vehicleId}/${cloudImageName}`)
+    );
+    const remainingImages = cloudImages.filter(
+      (image) => image.name !== cloudImageName
+    );
     setState((state) => remainingImages);
-    toast('Imagem deletada!', { className: 'success' });
+    toast("Imagem deletada!", { className: "success" });
   } catch ({ message, name }) {
-    toast('Houve um erro com a exclusão da imagem:\n' + `${message}:${name}`, {
-      className: 'error',
+    toast("Houve um erro com a exclusão da imagem:\n" + `${message}:${name}`, {
+      className: "error",
     });
   }
 };
@@ -121,13 +135,15 @@ export const deleteImageByURL = async (
   >
 ) => {
   try {
-    const deleteFile = await deleteObject(ref(storage, `${vehicleId}/${imageName}`));
+    const deleteFile = await deleteObject(
+      ref(storage, `${vehicleId}/${imageName}`)
+    );
     const remainingImages = Images.filter((image) => image.name !== imageName);
     setState((state) => remainingImages);
-    toast('Imagem Deletada!', { className: 'success' });
+    toast("Imagem Deletada!", { className: "success" });
   } catch ({ message, name }) {
-    toast('Houve um erro com a exclusão da imagem:\n' + `${message}:${name}`, {
-      className: 'error',
+    toast("Houve um erro com a exclusão da imagem:\n" + `${message}:${name}`, {
+      className: "error",
     });
   }
 };
@@ -136,27 +152,37 @@ export const deleteVehicleImagesFolder = async (vehicleId: string) => {
   try {
     const imagesList = await list(ref(storage, vehicleId));
     imagesList.items.forEach((image) => {
-      const deleteFile = deleteObject(ref(storage, `${vehicleId}/${image.name}`));
+      const deleteFile = deleteObject(
+        ref(storage, `${vehicleId}/${image.name}`)
+      );
     });
   } catch ({ message, name }) {
     toast(
-      'Houve um erro com a exclusão da pasta de imagem do veiculo:\n\n' + `${message}:${name}`,
-      { className: 'error' }
+      "Houve um erro com a exclusão da pasta de imagem do veiculo:\n\n" +
+        `${message}:${name}`,
+      { className: "error" }
     );
   }
 };
 
-export const uploadMainImage = async (vehicleId: string, mainImage: ImageFile[]) => {
+export const uploadMainImage = async (
+  vehicleId: string,
+  mainImage: ImageFile[]
+) => {
   try {
     const mainImageStorageRef = ref(storage, `${vehicleId}/mainImage`);
     const uploadTask = uploadBytesResumable(mainImageStorageRef, mainImage[0]);
     await uploadTask;
     const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-    return { name: 'mainImage', url: downloadURL };
+    return { name: "mainImage", url: downloadURL };
   } catch ({ message, name }) {
-    toast('Houve um erro com ao registrar a imagem principal:\n' + `${message}:${name}`, {
-      className: 'error',
-    });
+    toast(
+      "Houve um erro com ao registrar a imagem principal:\n" +
+        `${message}:${name}`,
+      {
+        className: "error",
+      }
+    );
   }
 };
 
@@ -172,8 +198,11 @@ export const uploadImages = async (vehicleId: string, images: ImageFile[]) => {
     }
     return urlArray;
   } catch ({ message, name }) {
-    toast('Houve um erro com ao registrar as imagens:\n' + `${message}:${name}`, {
-      className: 'error',
-    });
+    toast(
+      "Houve um erro com ao registrar as imagens:\n" + `${message}:${name}`,
+      {
+        className: "error",
+      }
+    );
   }
 };
